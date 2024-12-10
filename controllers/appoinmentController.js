52864ddf9e7f0f408c1f1d13faa1b9021a1e5f4d
ahadhossainaiman <ahadhossainaiman017@gmail.com>
2024-12-10 11:07:59 +0600

    [appointment controller bug fixed]

--- controllers/appoinmentController.js
+++ controllers/appoinmentController.js
@@ -71,14 +71,14 @@ const getAppointment = async (req, res) => {
         }
 
         for (let appointment of appointments) {
-            appointment.status = "upcomming";
-            // await appointment.save();
-            await PaymentModel.updateOne(
-              { _id: appointment._id },
-              { $set: { status: "upcomming" } }
-            );
-            // console.log("=================>>>>>", appointment?.status); // Await the save operation to handle async properly
-          }
+          appointment.status = "upcomming";
+          // await appointment.save();
+          await PaymentModel.updateOne(
+            { _id: appointment._id },
+            { $set: { status: "upcomming" } }
+          );
+          // console.log("=================>>>>>", appointment?.status); // Await the save operation to handle async properly
+        }
         const result = appointments.filter(
           (appointment) => appointment.status === "upcomming"
         );
@@ -167,13 +167,22 @@ const getAppointment = async (req, res) => {
             })
           );
         }
+
         for (let appointment of appointments) {
-          appointment.status = "active";
-          // await appointment.save();
-          await PaymentModel.updateOne(
-            { _id: appointment._id },
-            { $set: { status: "active" } }
-          );
+          if (!appointment.isCompleted) {
+            // appointment.status = "completed";
+            // await appointment.save();
+            appointment.status = "active";
+            // await appointment.save();
+            await PaymentModel.updateOne(
+              { _id: appointment._id },
+              { $set: { status: "active" } }
+            );
+
+            // console.log("=================>>>>>", appointment?.status);
+          }
+          // Await the save operation to handle async properly
+
           // console.log("=================>>>>>", appointment?.status); // Await the save operation to handle async properly
         }
 
@@ -190,8 +199,6 @@ const getAppointment = async (req, res) => {
             })
           );
         }
-
-        
 
         res.status(200).json(
           Response({
@@ -276,7 +283,7 @@ const getAppointment = async (req, res) => {
               { _id: appointment._id },
               { $set: { status: "completed" } }
             );
-            // console.log("=================>>>>>", appointment?.status);
+            console.log("=================>>>>>", appointment?.status);
           }
           // Await the save operation to handle async properly
         }
@@ -310,15 +317,13 @@ const getAppointment = async (req, res) => {
         );
       }
     } else {
-      res
-        .status(404)
-        .json(
-          Response({
-            message: "Invalid status",
-            status: "Failed",
-            statusCode: 404,
-          })
-        );
+      res.status(404).json(
+        Response({
+          message: "Invalid status",
+          status: "Failed",
+          statusCode: 404,
+        })
+      );
     }
   } catch (error) {
     // console.log(error?.message);
@@ -335,15 +340,13 @@ const getSingleAppointment = async (req, res) => {
     const userId = req.userId;
     const user = await User.findById(userId);
     if (!user) {
-      return res
-        .status(404)
-        .json(
-          Response({
-            message: "User not found",
-            status: "Failed",
-            statusCode: 404,
-          })
-        );
+      return res.status(404).json(
+        Response({
+          message: "User not found",
+          status: "Failed",
+          statusCode: 404,
+        })
+      );
     }
     // if(user?.role !== "doctor"){
     //     return res.status(404).json(Response({ message: "You are not authorized to perform this action", status: "Failed", statusCode: 403 }));
@@ -351,15 +354,13 @@ const getSingleAppointment = async (req, res) => {
     const appointmentId = req.params.appointmentId;
     // console.log("appointmentId", appointmentId);
     if (!appointmentId) {
-      return res
-        .status(404)
-        .json(
-          Response({
-            message: "Appointment not found",
-            status: "Failed",
-            statusCode: 404,
-          })
-        );
+      return res.status(404).json(
+        Response({
+          message: "Appointment not found",
+          status: "Failed",
+          statusCode: 404,
+        })
+      );
     }
     // console.log("appointmentId", appointmentId);
     const appointment = await PaymentModel.findById(appointmentId).populate(
@@ -376,40 +377,41 @@ const getSingleAppointment = async (req, res) => {
     // );
     const getPatientDetails = await DoctorPrescriptionModel.findOne({
       patientDetailsId: appointment.patientDetailsId._id,
+    });
 
-    })
+    console.log(
+      "getPatientDetails=========================>",
+      getPatientDetails
+    );
 
-
-    console.log("getPatientDetails=========================>", getPatientDetails);
-
-    let result = { ...appointment._doc, specialist: doctorDetails?.specialist, prescription:getPatientDetails?.file };
+    let result = {
+      ...appointment._doc,
+      specialist: doctorDetails?.specialist,
+      prescription: getPatientDetails?.file,
+    };
 
     // console.log("result=========================>", result);
     if (!appointment) {
-      return res
-        .status(404)
-        .json(
-          Response({
-            message: "Appointment not found",
-            status: "Failed",
-            statusCode: 404,
-          })
-        );
+      return res.status(404).json(
+        Response({
+          message: "Appointment not found",
+          status: "Failed",
+          statusCode: 404,
+        })
+      );
     }
     res
       .status(200)
       .json(Response({ data: result, status: "OK", statusCode: 200 }));
   } catch (error) {
     console.error(error?.message);
-    res
-      .status(500)
-      .json(
-        Response({
-          message: `${error?.message}`,
-          status: "Failed",
-          statusCode: 500,
-        })
-      );
+    res.status(500).json(
+      Response({
+        message: `${error?.message}`,
+        status: "Failed",
+        statusCode: 500,
+      })
+    );
   }
 };
 
@@ -454,44 +456,44 @@ const getAppointmentForDoctor = async (req, res) => {
           .skip((page - 1) * limit)
           .limit(limit);
 
-          if (!appointments || appointments.length === 0) {
-            return res.status(404).json(
-              Response({
-                statusCode: 404,
-                message: "Appointments not found",
-                status: "Failed",
-              })
-            );
-          }
-  
-          for (let appointment of appointments) {
-              appointment.status = "upcomming";
-              // await appointment.save();
-              await PaymentModel.updateOne(
-                { _id: appointment._id },
-                { $set: { status: "upcomming" } }
-              );
-              console.log("=================>>>>>", appointment?.status); // Await the save operation to handle async properly
-            }
-          const result = appointments.filter(
-            (appointment) => appointment.status === "upcomming"
-          );
-  
-          res.status(200).json(
+        if (!appointments || appointments.length === 0) {
+          return res.status(404).json(
             Response({
-              data: result,
-              status: "OK",
-              statusCode: 200,
-              pagination: {
-                totalPages: Math.ceil(result.length / limit),
-                currentPage: page,
-                prevPage: page > 1 ? page - 1 : null,
-                nextPage:
-                  page < Math.ceil(result.length / limit) ? page + 1 : null,
-                totalUsers: result.length,
-              },
+              statusCode: 404,
+              message: "Appointments not found",
+              status: "Failed",
             })
           );
+        }
+
+        for (let appointment of appointments) {
+          appointment.status = "upcomming";
+          // await appointment.save();
+          await PaymentModel.updateOne(
+            { _id: appointment._id },
+            { $set: { status: "upcomming" } }
+          );
+          console.log("=================>>>>>", appointment?.status); // Await the save operation to handle async properly
+        }
+        const result = appointments.filter(
+          (appointment) => appointment.status === "upcomming"
+        );
+
+        res.status(200).json(
+          Response({
+            data: result,
+            status: "OK",
+            statusCode: 200,
+            pagination: {
+              totalPages: Math.ceil(result.length / limit),
+              currentPage: page,
+              prevPage: page > 1 ? page - 1 : null,
+              nextPage:
+                page < Math.ceil(result.length / limit) ? page + 1 : null,
+              totalUsers: result.length,
+            },
+          })
+        );
       } catch (error) {
         console.error(error);
         res.status(500).json({
@@ -694,15 +696,13 @@ const getAppointmentForDoctor = async (req, res) => {
         );
       }
     } else {
-      res
-        .status(404)
-        .json(
-          Response({
-            message: "Invalid status",
-            status: "Failed",
-            statusCode: 404,
-          })
-        );
+      res.status(404).json(
+        Response({
+          message: "Invalid status",
+          status: "Failed",
+          statusCode: 404,
+        })
+      );
     }
   } catch (error) {
     console.log(error?.message);
